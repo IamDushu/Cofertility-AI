6972e2eeeceefce9fc1558f38be24404b9760918
IamDushu <hey.dushyanth@gmail.com>
2025-05-22 14:19:30 +0530

    App take down

--- src/app/(ai)/layout.tsx
+++ src/app/(ai)/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Old_Standard_TT } from "next/font/google";
 import "../globals.css";
-import Header from "../../components/Header";
-import { AnimatedSearchInput } from "@/src/components/AnimatedSearchInput";
-import { BackgroundGradient } from "@/src/components/BackgroundGradient";
-import { SidebarComponent } from "@/src/components/SidebarComponent";
+// import Header from "../../components/Header";
+// import { AnimatedSearchInput } from "@/src/components/AnimatedSearchInput";
+// import { BackgroundGradient } from "@/src/components/BackgroundGradient";
+// import { SidebarComponent } from "@/src/components/SidebarComponent";
 import { Analytics } from "@vercel/analytics/next";
+import { Lamp } from "@/src/components/Lamp";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -28,19 +29,18 @@ export const metadata: Metadata = {
   description: "Cofertility AI Search - Donor search made easy",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function RootLayout({}: // children,
+Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={`${oldStandardFont.variable}`}>
       <body>
-        <SidebarComponent>
+        <Lamp />
+        {/* <SidebarComponent>
           <main className="bg-[#f5f4ee] w-[100vw]">
             <Header />
             <div className="w-full p-5 pb-10 sticky top-21 z-50 ">
-              {/* <SearchInput /> */}
               <BackgroundGradient containerClassName="w-fit mx-auto p-0.5 rounded-full">
                 <AnimatedSearchInput
                   placeholders={[
@@ -69,7 +69,7 @@ export default function RootLayout({
             </div>
             {children}
           </main>
-        </SidebarComponent>
+        </SidebarComponent> */}
         <Analytics />
       </body>
     </html>
---
+++ src/components/Lamp.tsx
@@ -0,0 +1,176 @@
+"use client";
+import React from "react";
+import { motion } from "motion/react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { IconBrandLinkedinFilled, IconBrandX } from "@tabler/icons-react";
+
+export function Lamp() {
+  return (
+    <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Vibe Match Offline
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="md:mx-40 mx-10 mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-md font-medium tracking-tight text-transparent md:text-md"
+      >
+        This demo has been taken down in accordance with Cofertility’s Terms of
+        Use. It was privately shared with members of the Cofertility team to
+        present a conceptual idea for feedback and was never intended for public
+        access. Out of respect for their policies and community, the demo is no
+        longer available. Grateful for the conversations this sparked - wishing
+        the Cofertility team continued success in their meaningful work.
+        <br />—
+        <Link
+          href={"https://dushyanth.in"}
+          className="font-bold"
+          target="_blank"
+        >
+          Dushyanth
+        </Link>
+      </motion.p>
+      <motion.p
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="md:mx-40 flex gap-5  md:gap-10 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-md  tracking-tight text-transparent md:text-md"
+      >
+        <Link
+          href={"https://dushyanth.in"}
+          className="font-bold"
+          target="_blank"
+        >
+          dushyanth.in
+        </Link>
+        <span>|</span>
+        <Link
+          href={"https://dushyanth.in"}
+          className="font-bold"
+          target="_blank"
+        >
+          My Blog
+        </Link>
+        <span>|</span>
+
+        <Link
+          href={"https://dushyanth.in"}
+          className="font-bold"
+          target="_blank"
+        >
+          {" "}
+          <IconBrandX size={30} color="white" />
+        </Link>
+        <span>|</span>
+
+        <Link
+          href={"https://dushyanth.in"}
+          className="font-bold"
+          target="_blank"
+        >
+          <IconBrandLinkedinFilled size={30} color="white" />
+        </Link>
+      </motion.p>
+    </LampContainer>
+  );
+}
+
+export const LampContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full z-0",
+        className
+      )}
+    >
+      <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
+        <motion.div
+          initial={{ opacity: 0.5, width: "15rem" }}
+          whileInView={{ opacity: 1, width: "30rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+          }}
+          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+        >
+          <div className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0.5, width: "15rem" }}
+          whileInView={{ opacity: 1, width: "30rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+          }}
+          className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]"
+        >
+          <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+        </motion.div>
+        <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
+        <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
+        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl"></div>
+        <motion.div
+          initial={{ width: "8rem" }}
+          whileInView={{ width: "16rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"
+        ></motion.div>
+        <motion.div
+          initial={{ width: "15rem" }}
+          whileInView={{ width: "30rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 "
+        ></motion.div>
+
+        <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
+      </div>
+
+      <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
+        {children}
+      </div>
+    </div>
+  );
+};
